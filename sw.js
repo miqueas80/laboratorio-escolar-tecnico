28550b51bfc14ed1fc975c118a09cd8a51f6ff1d
miqueas80 <proyectomj11@gmail.com>
2026-09-10 10:37:42 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nexus-x-v2';
+const CACHE='nexus-x-v3-doc-api';
 const CORE=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg','./inventory.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
